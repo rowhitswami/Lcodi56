1c83ec55e3abc7e2b1cc9dc0080d0b6241208db6
Rohit Swami <rowhitswami1@gmail.com>
2018-04-24 01:31:12 +0530

    3rd Question Completed

---
+++ application.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 23 08:59:37 2018
+
+1.Which product was most sold?
+2.What payment modes were used for purchasing products?
+3.What is the most common payment method for the United States?
+4.What was the earliest time of the day a transaction occurred?
+5.Were there repeat customers?
+
+@author: Rohit Swami
+"""
+
+import pandas as pd
+
+df = pd.read_csv("dataset.csv")
+#print(df.columns)
+
+# Answering First Question
+print('Product1 was most sold')
+print(df['Product'].value_counts())
+
+
+# Answering Second Question
+payment_method = df['Payment_Type'].unique()
+print('\nThe payment modes used for purchasing products:')
+for i in range(len(payment_method)):
+    print(payment_method[i])
+print('\n')
+
+# Answering Third Question
+print('\nThe most common payment method for the United States:')
+print(df.groupby('Country')['Payment_Type'].value_counts().tail(4))
+
+# Answering Fourth Question
